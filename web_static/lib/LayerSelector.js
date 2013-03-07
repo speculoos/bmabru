fce7dd452541c548e9c28439414247c14347e680
Pierre Marchand <pierre@oep-h.com>
2013-03-07 10:00:05 +0100

    forgot files

--- web_static/lib/LayerSelector.js
+++ web_static/lib/LayerSelector.js
@@ -17,7 +17,6 @@ window.bMa.LayerSelector = function(container, map){
         },
         layer_added:function(name, layer){
             this.layers[name] = {layer:layer, selected:false};
-//             this.update(name);
             var that = this;
             this.selector.add_item(name, {
                 click:function(evt){
@@ -27,44 +26,6 @@ window.bMa.LayerSelector = function(container, map){
             
             that.select(name);
         },
-//         update:function(name){
-//             if(name === undefined)
-//             {
-//                 for(var lid in this.layers)
-//                 {
-//                     if(this.layers[lid].selected)
-//                     {
-//                         name = lid;
-//                         break;
-//                     }
-//                 }
-//             }
-//             this.container.empty();
-//             this.items = {};
-//             for(var lid in this.layers)
-//             {
-//                 var l = this.layers[lid];
-//                 var litem = $('<div class="layer-item">'+lid+'</div>');
-//                 if(lid === name)
-//                 {
-//                     litem.addClass('layer-selected');
-//                     l.selected = true;
-//                 }
-//                 else
-//                 {
-//                     l.selected = false;
-//                 }
-//                 var that = this;
-//                 litem.on('click', function(evt){
-//                     var elem = $(this);
-//                     if(elem.hasClass('layer-selected'))
-//                         return;
-//                     that.select(elem.text());
-//                 });
-//                 this.items[lid] = litem;
-//                 this.container.append(litem);
-//             }
-//         },
         select:function(name){
             for(var lid in this.layers)
             {
