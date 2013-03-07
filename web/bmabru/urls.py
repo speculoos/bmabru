479075cecf065c242dd36e921e1701cfa208cefd
Pierre Marchand <pierre@oep-h.com>
2013-03-07 08:44:12 +0100

    changed template directory

--- web/bmabru/urls.py
+++ web/bmabru/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('bmabru.views',
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name='index'),
     url(r'^project/(?P<project>.+)$', 'project'),
     url(r'^projects/$', 'projects'),
     url(r'^projects/json/$', 'projects_json'),
