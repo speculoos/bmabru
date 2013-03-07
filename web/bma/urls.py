7b0e1e7267b713e23a7d9c02fee8e0dbe885f0fa
Pierre Marchand <pierre@oep-h.com>
2013-03-07 13:23:20 +0100

    First bits of client-side translation/i18n

--- web/bma/urls.py
+++ web/bma/urls.py
@@ -4,7 +4,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+##js_info_dict = {
+    ##'domain': 'djangojs',
+    ##'packages': ('web',),
+##}
+
 urlpatterns = patterns('',
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog'),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/', include('media.urls')),
     url(r'^', include('bmabru.urls')),
