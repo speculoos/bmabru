03f6c2aef1b0815b33be7fad45d1dc3552e7edb5
Pierre Marchand <pierre@oep-h.com>
2013-03-10 17:18:49 +0100

    setup a whitelist for bma.views.config

--- web/bma/views.py
+++ web/bma/views.py
@@ -4,13 +4,24 @@ import json
 
 from django.http import HttpResponse, Http404
 from django.template import RequestContext
+from django.core.exceptions import PermissionDenied
 
 from bma import settings
 
+WHITE_LIST = (
+    'LANGUAGES',
+    'MEDIA_URL',
+    'STATIC_URL',
+    'DEBUG',
+    'TIME_ZONE',
+    )
 
 def config(req, q):
+    if q not in WHITE_LIST:
+        raise PermissionDenied
     try:
         data = json.dumps({q:getattr(settings,q)})
     except Exception as e:
         raise Http404('%s'%e)
     return HttpResponse(data, mimetype="application/json")
+    
