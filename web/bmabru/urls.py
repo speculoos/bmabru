7386fa093f572bac51d4a8437e8b19bdedae50f4
Pierre Marchand <pierre@oep-h.com>
2013-04-05 08:14:03 +0200

    fix bmabru urls

--- web/bmabru/urls.py
+++ web/bmabru/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from rest_framework.urlpatterns import format_suffix_patterns
-
 urlpatterns = patterns('bmabru.views',
     url(r'^$', 'index', name='index'),
+    url(r'^project/(?P<project>.+)$', 'project', name='project'),
+    url(r'^projects/$', 'projects', name='projects'),
+    url(r'^projects/json/$', 'projects_json'),
     url(r'^structure$', 'expose_structure'),
 )
-
