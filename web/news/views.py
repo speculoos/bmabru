2c441e4a854a635cfb3f003ea1d54e9048d341b4
Pierre Marchand <pierre@oep-h.com>
2013-05-15 21:31:13 +0200

    a better entry point for news admin

--- web/news/views.py
+++ web/news/views.py
@@ -7,10 +7,12 @@
 
 from django.views.generic.base import TemplateView
 from django.views.decorators.csrf import csrf_protect
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseForbidden, HttpResponseBadRequest, HttpResponseServerError, HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 import urllib
 from news.models import *
@@ -18,12 +20,17 @@ from news.models import *
 from hp_bs import Parser
 
 
-def render_body(req, id):
-    n = News.object.get(pk=id)
-    
+def news_login_req(func):
+    return login_required(func, login_url='/api-auth/login')
+    #return login_required(func, login_url=reverse_lazy('rest_framework.api-auth'))
+
     
 class IndexView(TemplateView):
     template_name = "news_index.html"
+    
+    @method_decorator(news_login_req)
+    def dispatch(self, request, *args, **kwargs):
+        return super(IndexView, self).dispatch(request, *args, **kwargs)
     
 def parser(req):
     url = req.POST.get('url', None)
