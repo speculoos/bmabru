093cb2799a2ad61ce8c782675d4b14f61854d884
Pierre Marchand <pierremarc07@gmail.com>
2013-11-04 17:55:07 +0100

    Fixed #27

--- web_static/news/views.js
+++ web_static/news/views.js
@@ -268,11 +268,11 @@
                 var i = new NEWS.Views.resources({model:item});
                 var el = i.render().el;
                 var $root = this.$el.find('.thumbnails');
-        if($root.length > 0)
-        {
-            $root.append(el);
-            this.items[item.cid] = i;
-        }
+                if($root.length > 0)
+                {
+                    $root.append(el);
+                    this.items[item.cid] = i;
+                }
             }
             return this;
         },
@@ -356,13 +356,26 @@
             'click .delete':'delete_news',
             'click .parser':'parser',
             'click .reset-project':'resetProject',
-            'change input, textarea': 'setChangeState',
+            'keyup input, textarea': 'setChangeState',
             'click .image-delete':'removeImage',
         },
-        setChangeState:function(){
+        setChangeState:function(evt){
+            var body_fr = this.$el.find('[name=body_fr]').val();
+            var title_nl = this.$el.find('[name=title_nl]').val();
+            var title_fr = this.$el.find('[name=title_fr]').val();
+            var body_nl = this.$el.find('[name=body_nl]').val();
+            
+            this.model.set({
+                body_fr:body_fr,
+                body_nl:body_nl,
+                title_fr:title_fr,
+                title_nl:title_nl,
+            },{silent:true});
             this._changed = true;
+            return true;
         },
         resetChangeState:function(){
+            
             this._changed = false;
         },
         resetProject:function(){
